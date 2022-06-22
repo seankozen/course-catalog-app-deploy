3f0b1f5f09c533ac706ffe92b769a47713a0935c
Sean Kozen <sgkozen@gmail.com>
2022-06-23 00:54:41 +0900

    revert

--- api/app.js
+++ api/app.js
@@ -64,18 +64,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.use(express.static('../client/build'));
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'), function(err) {
-	  if (err) {
-		res.status(500).send(err)
-	  }
-	})
-});
-
-
-
-
 // set our port
 app.set('port', process.env.PORT || 5000);
 
